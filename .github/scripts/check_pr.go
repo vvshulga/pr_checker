305d392b0194bda155e78e837652241f2f298344
Volodymyr Shulga <vshulga@siftscience.com>
2024-02-21 20:13:29 +0200

    update workflow - 2

--- .github/scripts/check_pr.go
+++ .github/scripts/check_pr.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/go-github/v57/github"
+	"github.com/sethvargo/go-githubactions"
+	"golang.org/x/oauth2"
+)
+
+type config struct {
+	token string
+}
+
+func initConfig() *config {
+	cfg := config{
+		token: githubactions.GetInput("repo-token"),
+	}
+	return &cfg
+}
+func newGithubClient(token string) *github.Client {
+	ctx := context.Background()
+
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+
+	return github.NewClient(tc)
+}
 
 func main() {
 	fmt.Println("Test message - go script was run successfully.")
+
+	cfg := initConfig()
+
+	client := newGithubClient(cfg.token)
+
 }
